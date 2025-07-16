72e2a82bb8fa8a821694ddc8832c3944823ee174
ArturGoz <arturrm4@gmail.com>
2025-07-16 20:58:12 +0300

    refactoring

--- src/main/test/java/com/team/calc/service/CalcServiceTest.java
+++ src/main/test/java/com/team/calc/service/CalcServiceTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class CalcServiceTest {
 
     private final CalcService service = new CalcService();
+    private final CalcService service2 = new CalcService();
 
     @ParameterizedTest
     @CsvSource({
@@ -17,7 +18,31 @@ class CalcServiceTest {
             "7, 10, -3",
             "100, 25, 75"
     })
+
     void testSubtract(Integer a, Integer b, Integer expected) {
+        Integer actual = service.subtract(a, b);
+        assertEquals(expected, actual, () -> a + " - " + b + " should equal " + expected);
+    }    @ParameterizedTest
+    @CsvSource({
+            "10, 5, 5",
+            "0, 0, 0",
+            "-3, -2, -1",
+            "7, 10, -3",
+            "100, 25, 75"
+    })
+
+    void testSubtract1(Integer a, Integer b, Integer expected) {
+        Integer actual = service.subtract(a, b);
+        assertEquals(expected, actual, () -> a + " - " + b + " should equal " + expected);
+    }    @ParameterizedTest
+    @CsvSource({
+            "10, 5, 5",
+            "0, 0, 0",
+            "-3, -2, -1",
+            "7, 10, -3",
+            "100, 25, 75"
+    })
+    void testSubtract2(Integer a, Integer b, Integer expected) {
         Integer actual = service.subtract(a, b);
         assertEquals(expected, actual, () -> a + " - " + b + " should equal " + expected);
     }
