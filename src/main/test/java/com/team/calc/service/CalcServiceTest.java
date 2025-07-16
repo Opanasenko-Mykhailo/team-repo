6946d6cd2b157aa7a15fefe53085bfaea75f1976
ArturGoz <arturrm4@gmail.com>
2025-07-16 21:04:46 +0300

    refactoring

--- src/main/test/java/com/team/calc/service/CalcServiceTest.java
+++ src/main/test/java/com/team/calc/service/CalcServiceTest.java
@@ -1,48 +1,5 @@
 package com.team.calc.service;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class CalcServiceTest {
-    private final CalcService calc = new CalcService();
-    private final CalcService service2 = new CalcService();
 
-    @ParameterizedTest
-    @CsvSource({
-            "10, 5, 5",
-            "0, 0, 0",
-            "-3, -2, -1",
-            "7, 10, -3",
-            "100, 25, 75"
-    })
-
-    void testSubtract(Integer a, Integer b, Integer expected) {
-        Integer actual = calc.subtract(a, b);
-        assertEquals(expected, actual, () -> a + " - " + b + " should equal " + expected);
-    }    @ParameterizedTest
-    @CsvSource({
-            "10, 5, 5",
-            "0, 0, 0",
-            "-3, -2, -1",
-            "7, 10, -3",
-            "100, 25, 75"
-    })
-
-    void testSubtract1(Integer a, Integer b, Integer expected) {
-        Integer actual = calc.subtract(a, b);
-        assertEquals(expected, actual, () -> a + " - " + b + " should equal " + expected);
-    }    @ParameterizedTest
-    @CsvSource({
-            "10, 5, 5",
-            "0, 0, 0",
-            "-3, -2, -1",
-            "7, 10, -3",
-            "100, 25, 75"
-    })
-    void testSubtract2(Integer a, Integer b, Integer expected) {
-        Integer actual = calc.subtract(a, b);
-        assertEquals(expected, actual, () -> a + " - " + b + " should equal " + expected);
-    }
 }
