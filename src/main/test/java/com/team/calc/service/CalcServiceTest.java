e76b4a009fac3a4082c384f9f210849af686c241
ArturGoz <arturrm4@gmail.com>
2025-07-16 20:06:09 +0300

    added tests for subtract

---
+++ src/main/test/java/com/team/calc/service/CalcServiceTest.java
@@ -0,0 +1,24 @@
+package com.team.calc.service;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalcServiceTest {
+
+    private final CalcService calcService = new CalcService();
+
+    @ParameterizedTest
+    @CsvSource({
+            "10, 5, 5",
+            "0, 0, 0",
+            "-3, -2, -1",
+            "7, 10, -3",
+            "100, 25, 75"
+    })
+    void testSubtract(Integer a, Integer b, Integer expected) {
+        Integer result = calcService.subtract(a, b);
+        assertEquals(expected, result, () -> a + " - " + b + " should equal " + expected);
+    }
+}
